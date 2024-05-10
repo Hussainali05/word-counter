c5cb46fe430320fdf8f9ec6852c452f4d65fab59
Hussainali05 <alimhussain375@gmail.com>
2024-05-10 06:48:44 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt
+(
+    [
+      {
+         type: "input",
+         name: "paragrph",
+         message: "Enter your paragraph or text",
+      },
+    ]
+);
+console.log("\n");
+let character = answer.paragrph.split(" ").join("");
+const words = answer.paragrph.trim().split(" ");
+console.log(
+  `Total words in your paragraph is ${words.length} & total alphabets in your paragraph is ${character.length}`
+);
